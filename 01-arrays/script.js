4bfa7440b7f8f157d41e4e98b495c7a2c3b7f744
mattiacoli <mattia.coli@aol.com>
2025-02-12 15:41:42 +0100

    add exercise 4

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -26,9 +26,14 @@ console.log(teachers);
 
 
 // 4. Rimuovi il primo insegnante dall'array e salvalo nella variabile firstTeacher qui sotto
-const firstTeacher = null;
+const firstTeacher = teachers[0];
+console.log(firstTeacher);
+teachers.shift()
+console.log(teachers);
+
 
 // 5. Aggiungi un insegnante di nome 'Vanessa' alla fine dell'array teachers
+
 
 // 6. Aggiungi un insegnante di nome 'Sarah' all'inizio dell'array teachers
 
